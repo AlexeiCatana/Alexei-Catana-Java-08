b61939a6a9608bca120737772199141f67a0f1fd
Alexei Catana <alexei.catana.bras@gmail.com>
2021-09-25 08:26:45 +0300

    Week04 - Factorial

---
+++ src/main/java/com/sci/week_four/Factorial.java
@@ -0,0 +1,22 @@
+package com.sci.week_four;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number");
+        long number = scanner.nextInt();
+
+        int i = 1;
+        long factorial = 1;
+
+        while (i <= number) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("Factorial number of " + number + " = " + factorial);
+    }
+}
