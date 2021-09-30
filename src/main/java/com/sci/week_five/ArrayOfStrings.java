52ce0b99c349d4b27afd7b57b6f4e436f6bb4738
Alexei Catana <alexei.catana.bras@gmail.com>
2021-10-01 00:35:29 +0300

    Week05 - ArrayOfStrings

--- src/main/java/com/sci/week_five/ArrayOfStrings.java
+++ src/main/java/com/sci/week_five/ArrayOfStrings.java
@@ -5,8 +5,11 @@ public class ArrayOfStrings {
     public static void main(String[] args) {
         String[] allStrings = new String[]{"George", "Mihai", "Raducanu"};
 
-            String oneString= "George"+" Mihai"+" Raducanu";
+        for (String element : allStrings) {
+            String oneString = "George" + " Mihai" + " Raducanu";
             System.out.println(oneString);
-    }
+            return;
+        }
 
+    }
 }
